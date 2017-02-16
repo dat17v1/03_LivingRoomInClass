834a37f9e994467572107d90b070c7fc3267e8b7
Christian Kirschberg <ckirschberg@gmail.com>
2017-02-16 13:08:28 +0100

    after classroom

--- LivingRoom.java
+++ LivingRoom.java
@@ -1,18 +1,56 @@
 public class LivingRoom {
   private String name;
   private int numberOfPeople = 0;
-  private String personsNameInChair;
-  private String personsNameInCouchPosition1;
-  private String personsNameInCouchPosition2;
+  private String personsNameInChair = "";
+  private String personsNameInCouchPosition1 = "";
+  private String personsNameInCouchPosition2 = "";
 
   public LivingRoom(String name){
     this.name = name;
   }
+  
+  public void setPersonsNameInCouchPosition1(String name) {
+    
+    // Hvis vi ingen mælk har => køb mælk
+    // Drik mælk
+
+    // if (*vi ingen mælk har*) {
+      //køb mælk
+    //}
+    // Drik mælk
+
+    if (this.personsNameInCouchPosition1.isEmpty()) {
+      numberOfPeople++;
+    }
+    this.personsNameInCouchPosition1 = name;
+
+
+  }
+  public void setPersonsNameInCouchPosition2(String name) {
+    if (this.personsNameInCouchPosition2.isEmpty()) {
+      numberOfPeople++;
+    }
+    this.personsNameInCouchPosition2 = name;
+  }
+  public void printNumberOfPeople() {
+    System.out.println(numberOfPeople);
+  }
+  
+  
+  // public void setName(String name) {
+  //   this.name = name;
+  // }
+
+
+
 
   public void setName(String name) {
     this.name = name;
   }
   public void setPersonsNameInChair(String name) {
+    if (this.personsNameInChair.isEmpty()) {
+      numberOfPeople = numberOfPeople +1;
+    }
     this.personsNameInChair = name;
   }
   
--- RunLivingRoom.java
+++ RunLivingRoom.java
@@ -5,6 +5,23 @@ public class RunLivingRoom {
     Person person1 = new Person("Andreas", 30);
     Person person2 = new Person("Mathias", 22);
     Person person3 = new Person("Magnus", 23);
+
+    // Klassenavn navn = new Klassenavn(*parametre*);
+    LivingRoom room = new LivingRoom("Dark Room");
     
+    // LivingRoom room2 = new LivingRoom();
+    
+    room.printNumberOfPeople();
+    room.setPersonsNameInChair("Mohammed");
+    room.setPersonsNameInCouchPosition1("Highone");
+    room.setPersonsNameInCouchPosition2("Hightwo");
+
+    // 17:
+    room.setPersonsNameInCouchPosition1("Highone");
+
+    // 18:
+    room.printNumberOfPeople();
+
+    // Prints 3 => We are happy :-)
   }
 }
